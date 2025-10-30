f6fc54fce692db638d4a3d59dd9cc8506dcef90d
qwertyuiop <121487905+BBBBCDEF@users.noreply.github.com>
2025-10-30 14:40:41 +0900

    update

--- src/lecture05_01.py
+++ src/lecture05_01.py
@@ -5,27 +5,29 @@ from my_module.K21999.lecture05_camera_image_capture import MyVideoCapture
 def lecture05_01():
 
     # カメラキャプチャ実行
-    #app = MyVideoCapture()
-    #app.run()
+    app = MyVideoCapture()
+    app.run()
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
+    #capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
 
-    # capture_img : cv2.Mat = "implement me"
+    capture_img : cv2.Mat = app.get_img()
 
-    g_hight, g_width, g_channel = google_img.shape
-    c_hight, c_width, c_channel = capture_img.shape
-    
+    g_height, g_width, g_channel = google_img.shape
+    c_height, c_width, c_channel = capture_img.shape
+
     print(google_img.shape)
     print(capture_img.shape)
 
     for x in range(g_width):
-        for y in range(g_hight):
+        for y in range(g_height):
             b, g, r = google_img[y, x]
             # もし白色(255,255,255)だったら置き換える
             if (b, g, r) == (255, 255, 255):
-                google_img[y, x] = capture_img[(int)(y * (c_hight / g_hight)), (int)(x * (c_width / g_width))]
+                #余りを求める
+                google_img[y, x] = capture_img[y % c_height, x % c_width]
     
-    cv2.imwrite('images/result.png', google_img)
+    # images/result.pngに結果を保存する。
+    cv2.imwrite('output_images/lecture05_01_k24145.png', google_img)
 
